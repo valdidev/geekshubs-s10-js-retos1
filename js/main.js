c8c7ff55f422b0d65755b62dfdf5d2701da9ed1c
valdidev <valdicode.fernando@gmail.com>
2022-10-03 23:54:23 +0200

    retos del 1 al 5 completados

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/* // 1
+var num1 = 20;
+var num2 = 40;
+
+if (num1 == num2) {
+    console.log("Las variables son iguales")
+} else if (num1 > num2) {
+    console.log(num1 + " es mayor que " + num2)
+} else {
+    console.log(num2 + " es mayor que " + num1)
+} */
+
+/* // 2
+var nombre = "Fernando";
+console.log("Bienvenido " + nombre); */
+
+/* // 3
+var nombre = prompt("Introduce tu nombre");
+console.log("Bienvenido " + nombre); */
+
+/* // 4
+    // parseDouble?
+var radioCirculo = prompt("Introduce el radio del círculo");
+const PI = 3.14;
+
+var areaCirculo = PI * (radioCirculo**2);
+console.log("El área del círculo es: " + areaCirculo); */
+
+/* // 5
+var num = prompt("Introduce un número a dividir entre 2");
+
+if (num % 2 === 0) {
+    console.log(num + " es divisible entre 2");
+} else {
+    console.log(num + " no es divisible entre 2");
+} */
+
