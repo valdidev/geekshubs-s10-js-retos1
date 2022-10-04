273df23c73abf7a1dc12fcf6af5edc42eae50f6a
valdidev <valdicode.fernando@gmail.com>
2022-10-04 17:09:10 +0200

    retos del 6 al 12

--- js/main.js
+++ js/main.js
@@ -12,7 +12,9 @@ if (num1 == num2) {
 
 /* // 2
 var nombre = "Fernando";
-console.log("Bienvenido " + nombre); */
+// console.log("Bienvenido " + nombre);
+console.log("Bienvenido",nombre);
+// console.log(`Bienvenido ${nombre}`); */
 
 /* // 3
 var nombre = prompt("Introduce tu nombre");
@@ -35,3 +37,78 @@ if (num % 2 === 0) {
     console.log(num + " no es divisible entre 2");
 } */
 
+/* // 6
+let precio = prompt("Introduce un precio de producto");
+const iva = precio * 0.21
+console.log("El precio con el IVA es: ", + precio + iva,"€"); */
+
+/* // 7
+let num = 1;
+while(num <= 100) {
+    console.log(num)
+    num++;
+} */
+
+/* // 8
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+} */
+
+/* // 9
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0 && i % 3 === 0) {
+        console.log(i);
+    }
+} */
+
+/* // 10
+let nVentas = parseInt(prompt("Introduce el número de ventas"));
+let total = 0;
+
+while(nVentas > 0) {
+    let venta = parseInt(prompt("Introduce el valor de la venta"));
+    total = total + venta
+    nVentas--;
+}
+
+console.log("El valor total de la venta es: ", total); */
+
+/* // 11
+let dia = prompt("Dime un día de la semana");
+switch(dia) {
+    case "lunes":
+    case "martes":
+    case "miercoles":
+    case "jueves":
+    case "viernes":
+        console.log(dia, "es laboral");
+    break;
+    
+    case "sabado":
+    case "domingo":
+        console.log(dia, "es festivo");
+    break;
+
+    default:
+        console.log("Escribe un día de la semana correctamente");
+    break;
+} */
+
+/* // 12
+const password = "hola";
+let intentos = 3;
+
+while(intentos > 0) {
+    intentos--;
+    let login = prompt("Introduce la contraseña");
+
+    if (login === password) {
+        console.log("Enhorabuena, estás logueado");
+        intentos = 0;
+    } else if (login !== password && intentos >= 1) {
+            console.log("Contraseña incorrecta, te quedan:", intentos,"intento/s");
+        
+    } else {
+        console.log("Expulsado!")
+    }
+} */
